22d84d4829a7dea2836c39a36a9ab5ad03a23e63
Wassek AL CHAHID <wassek.alchahid@gmail.com>
2020-05-23 17:00:19 +0200

    Add unity scripts and prefab objects

---
+++ Assets/Scenes/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float radius;
+    Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = Vector2.one.normalized;
+        radius = transform.localScale.x / 2; // half the width
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0) {
+            direction.y = -direction.y;
+        } 
+        if (transform.position.y > GameManager.topRight.y - radius && direction.y > 0) {
+            direction.y = -direction.y;
+        }
+
+        // Game over
+        if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0) {
+            Debug.Log("Right player wins!!");
+            
+            Time.timeScale = 0;
+            enabled = false;
+        } 
+        if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0) {
+            Debug.Log("Left player wins!!");
+
+            Time.timeScale = 0;
+            enabled = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Paddle") {
+            bool isRight = other.GetComponent<Paddle> ().isRight;
+
+            if (isRight && direction.x > 0) {
+                direction.x = -direction.x;
+            }
+            if (!isRight && direction.x < 0) {
+                direction.x = -direction.x;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scenes/Paddle.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    public float speed;
+    float height;
+
+    string input;
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+        speed = 10f;
+    }
+
+    public void Init(bool isRightPaddle) {
+        isRight = isRightPaddle;
+        Vector2 pos = Vector2.zero;
+
+        if (isRightPaddle) {
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos += Vector2.left * transform.localScale.x;
+
+            input = "PaddleRight";
+        }
+        else {
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+
+            input = "PaddleLeft";
+        }
+
+        // Update this paddle's position
+        transform.position = pos;
+        transform.name = input;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // GetAxis is a number between -1 to 1 (-1 for down, 1 for up)
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        // Restrict paddle movement
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0) {
+            move = 0;
+        }
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0) {
+            move = 0;
+        }
+
+        transform.Translate(move * Vector2.up);
+    }
+}
