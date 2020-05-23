70a8f608e5a9fa47b416814f5c0ab30055a89294
Wassek AL CHAHID <wassek.alchahid@gmail.com>
2020-05-23 21:10:49 +0200

    Reset ball to initial position once a player score

--- Assets/Scenes/Ball.cs
+++ Assets/Scenes/Ball.cs
@@ -6,21 +6,23 @@ public class Ball : MonoBehaviour
 {
     [SerializeField]
     float speed;
+    int isMoving = 1;
     float radius;
     Vector2 direction;
+    Vector2 startPosition;
 
     // Start is called before the first frame update
     void Start()
     {
         direction = Vector2.one.normalized;
         radius = transform.localScale.x / 2; // half the width
-        
+        startPosition = new Vector2(transform.position.x, transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(direction * speed * Time.deltaTime);
+        transform.Translate(direction * speed * isMoving * Time.deltaTime);
 
         if (transform.position.y < GameManager.bottomLeft.y + radius && direction.y < 0) {
             direction.y = -direction.y;
@@ -31,17 +33,20 @@ public class Ball : MonoBehaviour
 
         // Game over
         if (transform.position.x < GameManager.bottomLeft.x + radius && direction.x < 0) {
-            Debug.Log("Right player wins!!");
-            
-            Time.timeScale = 0;
-            enabled = false;
+            StartCoroutine( Reset() );
         } 
         if (transform.position.x > GameManager.topRight.x - radius && direction.x > 0) {
-            Debug.Log("Left player wins!!");
-
-            Time.timeScale = 0;
-            enabled = false;
+            StartCoroutine( Reset() );
         }
+    }
+
+    IEnumerator Reset() {
+        isMoving = 0;
+        transform.position = startPosition;
+
+        yield return new WaitForSeconds(2);
+
+        isMoving = 1;
     }
 
     void OnTriggerEnter2D(Collider2D other) {
